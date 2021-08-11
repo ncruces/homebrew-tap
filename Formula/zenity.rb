f99341fa93d272992fd55c97296b73705bff85fb
Nuno Cruces <ncruces@users.noreply.github.com>
2021-08-11 23:47:35 +0100

    Update zenity.

--- Formula/zenity.rb
+++ Formula/zenity.rb
@@ -2,12 +2,13 @@ class Zenity < Formula
   desc "Dialog boxes for the command-line"
   homepage "https://pkg.go.dev/github.com/ncruces/zenity"
 
-  url "https://github.com/ncruces/zenity/archive/refs/tags/v0.7.9.tar.gz"
-  sha256 "db5f320db0ac372ef6010dc77fb406eb5b6c6da0b4d5730ad2e870c83db54960"
+  url "https://github.com/ncruces/zenity/archive/refs/tags/v0.7.10.tar.gz"
+  sha256 "570e5f36f51000dac4a8a7fb72b049d79f09fdcf121bb27c84c6ea72caa6e9a5"
   head "https://github.com/ncruces/zenity"
 
   bottle do
     root_url "https://github.com/ncruces/homebrew-tap/releases/download/zenity-0.7.9"
+    sha256 cellar: :any_skip_relocation, big_sur:      "fdc0e372c82097f78ba50dc87ea607829057af430fcc29a11340263c403931e8"
     sha256 cellar: :any_skip_relocation, catalina:     "2af7d76e4ae2e8d70a8d649c0db98072ae40b7e1d4c1b0e590b4fdd88ca77dd4"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "6e9124189c567b287ed8a9f9f5768bfd7c4f2d414aeb32b811ab7f5154034910"
   end
@@ -36,7 +37,7 @@ class Zenity < Formula
     bin_path = buildpath/"src/github.com/ncruces/zenity"
     bin_path.install Dir["*"]
     cd bin_path do
-      system "go", "build", "-ldflags=-s -w", "-trimpath", "-o", target, "./cmd/zenity"
+      system "go", "build", "-ldflags=-s -w -X main.version=v0.7.10", "-trimpath", "-o", target, "./cmd/zenity"
     end
   end
 
