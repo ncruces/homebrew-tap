9b3d78b769f176736437c992f1ad48ba11fccf01
Nuno Cruces <ncruces@users.noreply.github.com>
2022-12-18 00:17:09 +0000

    Update rethinkraw.rb

--- Casks/rethinkraw.rb
+++ Casks/rethinkraw.rb
@@ -1,8 +1,7 @@
 cask "rethinkraw" do
   version "0.10.0"
   sha256 "5e3ee0c8f1e1056205735a86b586387a2a687cd6eba3c6dc1db3049edbb42582"
-
-  url "https://rethinkraw.com/releases/download/v#{version}/RethinkRAW.dmg"
+  url "https://github.com/ncruces/RethinkRAW/releases/download/v#{version}/RethinkRAW.dmg"
   name "RethinkRAW"
   desc "RAW photo editor"
   homepage "https://rethinkraw.com/"
